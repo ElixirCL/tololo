497e542f7c0038c029318570f411df365241e1b1
rekkice <lekkice0@gmail.com>
2025-01-06 18:29:31 -0300

    added devenv decision and file

---
+++ devenv.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }: 
+
+{ 
+  languages.elixir.enable = true;
+  packages = [
+    pkgs.inotify-tools
+    pkgs.docker_25
+    pkgs.elixir-ls
+    pkgs.gnumake
+  ];
+
+  services.postgres = {
+    enable = true;
+    initialScript = ''
+      CREATE ROLE postgres WITH SUPERUSER LOGIN PASSWORD 'postgres';
+    '';
+  };
+}
