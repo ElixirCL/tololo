e5548dbbb9bc4ed11f7623598986ded238bd41eb
rekkice <lekkice0@gmail.com>
2025-01-07 14:46:07 -0300

    added Antora to devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -3,14 +3,26 @@
 { 
   devcontainer.enable = true;
 
-  languages.elixir.enable = true;
+  languages.elixir = {
+    enable = true;
+    package = pkgs.elixir;
+  };
   
   # https://devenv.sh/common-patterns/#configure-the-shell-based-on-the-current-machine
   packages = [
     pkgs.gnumake
+    pkgs.antora
+    pkgs.python314
   ] ++ lib.optionals pkgs.stdenv.isLinux [
     pkgs.inotify-tools
   ];
+
+  # for Antora extensions
+  languages.javascript.enable = true;
+  languages.javascript.yarn = {
+    enable = true;
+    install.enable = true;
+  };
 
   services.postgres = {
     enable = true;
