d52ead0132e7610cd985c4dc646ade864111fd77
rekkice <lekkice0@gmail.com>
2025-01-15 16:24:14 -0300

    fix: reverted deleted files and fixed tests

--- tololo/mix.exs
+++ tololo/mix.exs
@@ -26,7 +26,7 @@ defmodule Tololo.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
