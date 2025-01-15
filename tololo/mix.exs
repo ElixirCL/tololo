060f4b0c8705949bde753bc2aace34d737897295
Camilowser <camilo@ninjas.cl>
2025-01-15 13:22:06 -0300

    chore: added missing id

--- tololo/mix.exs
+++ tololo/mix.exs
@@ -4,8 +4,8 @@ defmodule Tololo.MixProject do
   def project do
     [
       app: :tololo,
-      version: "0.1.0",
-      elixir: "~> 1.14",
+      version: "1.0.0",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :dev,
