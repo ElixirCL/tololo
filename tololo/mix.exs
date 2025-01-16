faf214717cf18506481134c97c686841d20cb260
Camilowser <camilo@ninjas.cl>
2025-01-16 12:07:29 -0300

    chore: improved version

--- tololo/mix.exs
+++ tololo/mix.exs
@@ -45,7 +45,7 @@ defmodule Tololo.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.0.0"},
+      {:phoenix_live_view, "~> 1.0.1"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
