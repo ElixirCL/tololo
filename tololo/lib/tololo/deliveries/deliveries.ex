9241cd2b5f347e258b3bf11d1a24752bd34bf1e2
rekkice <lekkice0@gmail.com>
2025-01-24 11:59:40 -0300

    feat: added Ash admin dashboard

--- tololo/lib/tololo/deliveries/deliveries.ex
+++ tololo/lib/tololo/deliveries/deliveries.ex
@@ -2,7 +2,12 @@ defmodule Tololo.Deliveries do
   @moduledoc """
   Domain that contains resources related to the delivery system.
   """
-  use Ash.Domain, otp_app: :tololo, extensions: [AshGraphql.Domain]
+  use Ash.Domain, otp_app: :tololo, extensions: [AshGraphql.Domain, AshAdmin.Domain]
+
+  admin do
+    show?(true)
+    show_resources Tololo.Deliveries.Delivery
+  end
 
   resources do
     resource Tololo.Deliveries.Delivery
