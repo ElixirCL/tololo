c6cdc6e34f2c5f5346f9d30ef5179f1d3b6f55ec
rekkice <lekkice0@gmail.com>
2025-01-28 10:12:12 -0300

    feat: added mix task that dynamically generates a new Bruno environment

---
+++ tololo/lib/mix/generate_bruno_env.ex
@@ -0,0 +1,40 @@
+defmodule Mix.Tasks.Generate.Bruno.Env do
+  use Mix.Task
+
+  @moduledoc """
+  A custom Mix task that initializes a delivery then generates a Bruno environment file for it.
+
+  ## Usage
+
+      mix generate.bruno.env (optional dir)
+  """
+
+  @template """
+  vars {
+    gql_host: 127.0.0.1
+    gql_port: 4000
+    <%= for {key, value} <- extra_vars do %>
+    <%= key %>: <%= value %>
+    <% end %>
+  }
+  """
+  @impl Mix.Task
+  def run(args) do
+    dir = List.first(args) || "../collections/environments"
+    Mix.Task.run("app.start")
+
+    %{id: id, public_auth_key: public_auth_key, private_auth_key: private_auth_key} =
+      Tololo.Deliveries.Delivery.empty!(authorize?: false)
+
+    output =
+      EEx.eval_string(@template,
+        extra_vars: %{
+          id: id,
+          public_auth_key: public_auth_key,
+          private_auth_key: private_auth_key
+        }
+      )
+
+    File.write!(dir <> "/generated_env.bru", output)
+  end
+end
