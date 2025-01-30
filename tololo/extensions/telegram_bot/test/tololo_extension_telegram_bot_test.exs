179be7d40bdd2cda983f323ec6292172304dbda3
Camilo <camilo@ninjas.cl>
2025-01-29 21:25:16 -0300

    fixed tests

--- tololo/extensions/telegram_bot/test/tololo_extension_telegram_bot_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TololoExtensionTelegramBotTest do
-  use ExUnit.Case
-  doctest TololoExtensionTelegramBot
-
-  test "greets the world" do
-    assert TololoExtensionTelegramBot.hello() == :world
-  end
-end
