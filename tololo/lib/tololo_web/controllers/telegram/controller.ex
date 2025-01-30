5eb715e554a3b43abc667a4149baed2181206615
Camilo <camilo@ninjas.cl>
2025-01-29 22:41:14 -0300

    fixed credo warnings

--- tololo/lib/tololo_web/controllers/telegram/controller.ex
+++ tololo/lib/tololo_web/controllers/telegram/controller.ex
@@ -1,4 +1,7 @@
 defmodule TololoWeb.TelegramBot.Controller do
+
+  require Logger
+
   use TololoWeb, :controller
 
   alias Tololo.Extensions.TelegramBot.Handler
@@ -8,9 +11,11 @@ defmodule TololoWeb.TelegramBot.Controller do
   end
 
   def update(conn, params) do
-    IO.inspect(params, label: "params", limit: :infinity, pretty: true)
+    Logger.debug(params)
+
     update = Telegex.Helper.typedmap(params, Telegex.Type.Update)
-    |> IO.inspect(label: "update", limit: :infinity, pretty: true)
+
+    Logger.debug(update)
 
     try do
       Handler.on_update(update)
