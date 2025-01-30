158d3770862e2daf00db26610df17ae3478566fe
Camilo <camilo@ninjas.cl>
2025-01-29 22:08:27 -0300

    fixed handler

--- tololo/extensions/telegram_bot/lib/telegram/handler.ex
+++ tololo/extensions/telegram_bot/lib/telegram/handler.ex
@@ -1,29 +1,26 @@
 defmodule Tololo.Extensions.TelegramBot.Handler do
   require Logger
 
-  @impl true
   def on_boot do
     # read some parameters from your env config
     env_config = Application.get_env(:tololo, __MODULE__)
 
     # delete the webhook and set it again
     # set the webhook (url is required)
-    with {:ok, true} = Telegex.delete_webhook()
+    try do
+    {:ok, true} = Telegex.delete_webhook()
     {:ok, true} = Telegex.set_webhook(env_config[:webhook_url])
-    do
       Logger.info("Telegram Webhook initialized")
-    else
+    rescue
       _ -> Logger.info("Telegram Webhook not set")
     end
   end
 
-  @impl true
   def on_update(update) do
     Logger.debug(update)
     :ok
   end
 
-  @impl true
   def on_failure(update, e) do
     Logger.error("Uncaught Error: #{inspect(update_id: update.update_id, error: e)}")
   end
