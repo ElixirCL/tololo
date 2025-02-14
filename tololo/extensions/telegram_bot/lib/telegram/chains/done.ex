ff3582dc7ca49013670920557ad9c4643534f6f6
rekkice <lekkice0@gmail.com>
2025-02-14 15:12:24 -0300

    chore: change /done so it only shows deliveries within range and sorted by distance

--- tololo/extensions/telegram_bot/lib/telegram/chains/done.ex
+++ tololo/extensions/telegram_bot/lib/telegram/chains/done.ex
@@ -13,6 +13,7 @@ defmodule Tololo.Extensions.TelegramBot.Done do
   alias TololoCore.Deliveries.Delivery
 
   @actor TololoCore.Deliveries.Actors.private()
+  @min_done_distance Application.compile_env(:tololo, :min_done_distance_meters)
 
   @impl true
   def match?(%{text: @command, chat: %{type: "private"}}, _context), do: true
@@ -21,40 +22,13 @@ defmodule Tololo.Extensions.TelegramBot.Done do
   @impl true
   def match?(_, _), do: false
 
-  defp mark_delivery_done(delivery) do
-    Logger.info("Marking #{delivery.display_id} delivery as done")
-
-    %{state: new_state} =
-      delivery
-      |> Delivery.done_with_distance_check!(nil, actor: @actor)
-
-    if new_state == "Delivery_Done" do
-      gettext("""
-      Delivery successfully marked as done.
-      """)
-    else
-      gettext("""
-      Delivery marked with problems. An admin will review it first.
-      """)
-    end
-  end
-
   # matches when there's no active deliveries
   @impl true
   def handle(
         %{from: %{id: user_id}, text: _text},
         %{user_resource: %{deliveries: []}} = context
-      ) do
-    message =
-      Message.send_message(
-        user_id,
-        gettext("""
-        You don't have any active deliveries to mark as done.
-        """)
-      )
-
-    {:done, %{context | payload: message}}
-  end
+      ),
+      do: response_from_available([], context, user_id)
 
   @impl true
   def handle(
@@ -85,11 +59,14 @@ defmodule Tololo.Extensions.TelegramBot.Done do
   @impl true
   def handle(
         %{from: %{id: user_id}, text: _text},
-        %{user_resource: %{deliveries: [current_delivery]}} = context
+        %{user_resource: %{deliveries: [delivery]}} = context
       ) do
-    message = Message.send_message(user_id, mark_delivery_done(current_delivery))
-
-    {:done, %{context | payload: message}}
+    if get_current_to_distance(delivery) < @min_done_distance do
+      [delivery]
+    else
+      []
+    end
+    |> response_from_available(context, user_id)
   end
 
   # matches when there's no token in the command and multiple deliveries
@@ -98,8 +75,35 @@ defmodule Tololo.Extensions.TelegramBot.Done do
         %{from: %{id: user_id}, text: _text},
         %{user_resource: %{deliveries: current_deliveries}} = context
       ) do
-    current_deliveries_buttons =
-      Enum.map(current_deliveries, fn delivery ->
+    available_to_mark =
+      current_deliveries
+      |> Enum.filter(fn delivery -> get_current_to_distance(delivery) < @min_done_distance end)
+      |> Enum.sort(&(get_current_to_distance(&1) < get_current_to_distance(&2)))
+
+    response_from_available(available_to_mark, context, user_id)
+  end
+
+  defp response_from_available([], context, user_id) do
+    message =
+      Message.send_message(
+        user_id,
+        gettext("""
+        You don't have any active deliveries to mark as done. Check that you're within the minimum delivery range.
+        """)
+      )
+
+    {:done, %{context | payload: message}}
+  end
+
+  defp response_from_available([current_delivery] = available_to_mark, context, user_id) do
+    message = Message.send_message(user_id, mark_delivery_done(current_delivery))
+
+    {:done, %{context | payload: message}}
+  end
+
+  defp response_from_available(available_to_mark, context, user_id) do
+    available_deliveries_buttons =
+      Enum.map(available_to_mark, fn delivery ->
         %KeyboardButton{
           text: "#{@command} " <> delivery.display_id
         }
@@ -113,9 +117,35 @@ defmodule Tololo.Extensions.TelegramBot.Done do
 
         Please select the delivery you wish to mark as done
         """),
-        current_deliveries_buttons
+        available_deliveries_buttons
       )
 
     {:done, %{context | payload: message}}
   end
+
+  defp mark_delivery_done(delivery) do
+    Logger.info("Marking #{delivery.display_id} delivery as done")
+
+    %{state: new_state} =
+      delivery
+      |> Delivery.done_with_distance_check!(nil, actor: @actor)
+
+    if new_state == "Delivery_Done" do
+      gettext("""
+      Delivery successfully marked as done.
+      """)
+    else
+      gettext("""
+      Delivery marked with problems. An admin will review it first.
+      """)
+    end
+  end
+
+  defp get_current_to_distance(%{
+         current_latitude: current_lat,
+         current_longitude: current_lng,
+         to_latitude: to_lat,
+         to_longitude: to_lng
+       }),
+       do: TololoCore.Location.distance({current_lat, current_lng}, {to_lat, to_lng})
 end
