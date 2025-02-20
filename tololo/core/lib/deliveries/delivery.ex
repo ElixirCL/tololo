a5d7b12d89c8740db78f876f60c45c632ac541d7
rekkice <lekkice0@gmail.com>
2025-02-20 12:26:15 -0300

    feat: implement JSON encoder for Delivery

--- tololo/core/lib/deliveries/delivery.ex
+++ tololo/core/lib/deliveries/delivery.ex
@@ -400,3 +400,29 @@ defmodule TololoCore.Deliveries.Delivery do
     end
   end
 end
+
+defimpl Jason.Encoder, for: TololoCore.Deliveries.Delivery do
+  def encode(value, opts) do
+    Jason.Encode.map(
+      Map.take(value, [
+        :id,
+        :display_id,
+        :delivery_person,
+        :delivery_order,
+        :from_name,
+        :to_name,
+        :from_latitude,
+        :from_longitude,
+        :current_latitude,
+        :current_longitude,
+        :to_latitude,
+        :to_longitude,
+        :to_address,
+        :to_phone,
+        :to_notes,
+        :state
+      ]),
+      opts
+    )
+  end
+end
